aaaa48bae3d3f1060c40ffef2f729b79d71918c4
David Wilcox <david@dtwil.co>
2020-11-01 08:02:54 -0600

    Added main menu and save game support

--- src/engine.py
+++ src/engine.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
+import lzma
+import pickle
+
 from typing import Tuple, TYPE_CHECKING
 
 from tcod.console import Console
 from tcod.map import compute_fov
 
 import exceptions
-from input_handlers import MainGameEventHandler
 from message_log import MessageLog
 from render_functions import render_bar, render_names_at_location
 
 if TYPE_CHECKING:
     from entity import Actor
     from game_map import GameMap
-    from input_handlers import EventHandler
 
 
 class Engine:
@@ -24,7 +25,6 @@ class Engine:
     game_map: GameMap
 
     def __init__(self, player: Actor):
-        self.event_handler: EventHandler = MainGameEventHandler(self)
         self.message_log: MessageLog = MessageLog()
         self.mouse_location: Tuple[int, int] = (0, 0)
         self.player: Actor = player
@@ -59,3 +59,9 @@ class Engine:
         )
 
         render_names_at_location(console=console, x=21, y=44, engine=self)
+
+    def save_as(self, filename: str) -> None:
+        """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
+        with open(filename, "wb") as f:
+            f.write(save_data)
